72704cd449f62a79d0560ab6e32baf7e82027641
yoshinkun2210 <yoshimkun22@gmail.com>
2022-07-25 14:25:26 +0700

    baitap

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float a = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i =" + i);
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        System.out.println("s =" + s);
+    }
+}
